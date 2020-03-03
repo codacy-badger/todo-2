322820a025456aa09b319d3b7b70e1e2a95ab85a
Salvatore Trincone <salvatore.trincone@itpas.it>
2020-03-03 18:20:56 +0100

    creazione post

--- src/main/java/it/itpas/todo/todoApp/controllers/ItemController.java
+++ src/main/java/it/itpas/todo/todoApp/controllers/ItemController.java
@@ -1,16 +1,22 @@
 package it.itpas.todo.todoApp.controllers;
 
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import it.itpas.todo.todoApp.models.BaseEntity;
 import it.itpas.todo.todoApp.models.Item;
 import it.itpas.todo.todoApp.viewModels.ItemViewModel;
 import lombok.val;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 @val
@@ -21,34 +27,71 @@ public class ItemController {
     @GetMapping("/")
     public ResponseEntity<?> GetAllItems() {
         try {
-            val item = new Item("t1","ciao");
-            val item2 = new Item("t2","ciaociao");
-            val item3 =new Item("t3","ciao3");
 
-            val items = new ArrayList<Item>();
-            items.add(item);
-            items.add(item2);
-            items.add(item3);
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader("item.json"));
+            JSONArray jsonArray = (JSONArray) obj;
+            val items = new ArrayList<BaseEntity>();
 
-            return new ResponseEntity<>(items,HttpStatus.OK);
+            jsonArray.forEach(array -> {
+                JSONObject jsonObj = (JSONObject) array;
+                BaseEntity item = new Item(jsonObj.getAsString("nome"),jsonObj.getAsString("description"));
+                items.add(item);
+            });
+
+            return new ResponseEntity<>(items, HttpStatus.OK);
+
+
+
+
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> GetItem(@PathVariable int id) {
+    @GetMapping("/{nome}")
+    public ResponseEntity<?> GetItem(@PathVariable String nome) {
 
         try {
-            if (id < 1) throw new Exception();
-            val item = new Item("ciao", "cicocioais");
-            item.setId(id);
 
-            return new ResponseEntity<>(item,HttpStatus.OK);
+            System.out.println(nome);
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader("item.json"));
+            JSONArray jsonArray = (JSONArray) obj;
+            val items = new ArrayList<BaseEntity>();
+
+            jsonArray.forEach(array -> {
+                JSONObject jsonObj = (JSONObject) array;
+                if (jsonObj.getAsString("nome").compareTo(nome)==0) {
+                    BaseEntity item = new Item(jsonObj.getAsString("nome"), jsonObj.getAsString("description"));
+                    items.add(item);
+                }
+
+            });
+
+            return  new ResponseEntity<>(items, HttpStatus.OK);
 
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
         }
+
+
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> postItem (@RequestBody  JSONObject item) {
+
+        try {
+            File file = new File("item.json");
+            FileWriter fw = new FileWriter(file, true);
+            fw.write(item.toJSONString());
+
+        } catch (IOException e) {
+            return  new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(item,HttpStatus.OK);
+
 
     }
 }
